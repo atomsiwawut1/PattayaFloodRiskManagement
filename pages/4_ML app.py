12a8a43e0b9e1d296b6ab781b2efdf3697f3df0e
“atomsiwawut1 <atomsiwawut@gmail.com>
2023-05-12 10:41:48 +0700

    “Update

--- pages/4_ML app.py
+++ pages/4_ML app.py
@@ -17,6 +17,7 @@ from io import BytesIO
 import pickle
 import requests
 import numpy as np
+import plotly.graph_objects as go
 
 st.set_page_config(page_title='Flood Risk MAP BY ML',page_icon=":robot_face:",layout='wide')
 css_file="styles/main.css"
@@ -50,24 +51,24 @@ Landuse_map= {
 
 st.header("Flood Risk assessment")
 
-empty1,content1,empty2,content2,empty3=st.columns([0.1,7,0.1,6,0.1])
+empty1,content1,empty2,content2,empty3,content3,empty4=st.columns([0.1,1,0.1,1,0.1,1.5,0.1])
 
 
 
 with content1:
-    
+	st.markdown("<h4>Hazard factors</h4>", unsafe_allow_html=True)
 	Annual_Rain = st.number_input("Annual_Rain",0,3000)
-	Annual_Rain_slider = st.slider('Annual Rainfall : Slider bar', 0, 3000, Annual_Rain)
+	Annual_Rain_slider = st.slider('', 0, 3000, Annual_Rain)
 	Annual_Rain = Annual_Rain_slider
 	st.write(Annual_Rain)
 	
 	Degree_Slope = st.number_input("Slope",0,3000)
-	Degree_Slope_slider = st.slider('Slope : Slider bar', 0, 100, Degree_Slope)
+	Degree_Slope_slider = st.slider('""', 0, 100, Degree_Slope)
 	Degree_Slope = Degree_Slope_slider
 	st.write(Degree_Slope)
 	
 	Elevation = st.number_input("Elevation",0,3000)
-	Elevation_slider = st.slider('Elevation: Slider bar', 0, 1000, Elevation)
+	Elevation_slider = st.slider('""', 0, 1000, Elevation)
 	Elevation = Elevation_slider
 	st.write(Elevation)
 
@@ -80,41 +81,25 @@ with content1:
 		
 	
 with content2:
-	Landuse_input = st.selectbox("Select Landuse", options=list(Landuse_map.keys()))
+	st.markdown("<h4>Vulnerability factors</h4>", unsafe_allow_html=True)
+	Landuse_input = st.selectbox("Landuse", options=list(Landuse_map.keys()))
 	Landuse_number = Landuse_map[Landuse_input]
 	Landuse = Landuse_number
 	st.write(Landuse)
 	
-	FAR = st.number_input("FAR",0,10)
-	FAR_slider = st.slider('FAR : Slider bar', 0, 10, FAR)
+	FAR = st.number_input("FAR (Floor to Area Ratio)",0,10)
+	FAR_slider = st.slider('', 0, 10, FAR)
 	FAR= FAR_slider
 	st.write(FAR)
 	
-	BCR = st.number_input("BCR",0,100)
-	BCR_slider = st.slider('BCR : Slider bar', 0, 100, BCR )
+	BCR = st.number_input("Building Coverage Ratio (BCR)",0,100)
+	BCR_slider = st.slider('', 0, 100, BCR )
 	BCR= BCR_slider
 	st.write(BCR)
 
 
 
-data = pd.DataFrame(columns=['Annual_Rain', 'Soil_Drainage_Capacity', 'Elevation', 'Degree_Slope', 'Landuse', 'FAR', 'BCR'])
 
-data = data.append({
-        'Annual_Rain': Annual_Rain,
-        'Soil_Drainage_Capacity': Soil_Drainage_Capacity,
-        'Elevation': Elevation,
-        'Degree_Slope': Degree_Slope,
-        'Landuse': Landuse,
-        'FAR': FAR,
-        'BCR': BCR
-    }, ignore_index=True)
-
-st.dataframe(data)
-
-
-feature_cols=['Annual_Rain','Elevation','Degree_Slope','Landuse','Soil_Drainage_Capacity','BCR','FAR']
-
-X=data[feature_cols]
 
 #MLmodel="https://raw.githubusercontent.com/atomsiwawut1/FloodPrediction/453a18c6d71b18d9b0dd55c62a8a95a489aeb557/GIS_DATA/Flood%20Hazard%20Map.joblib"
 
@@ -135,14 +120,86 @@ X=data[feature_cols]
 
 # mLink = 'https://github.com/atomsiwawut1/FloodPrediction/blob/main/GIS_DATA/FloodModel.pkl?raw=true'
 
-mLink = 'https://github.com/atomsiwawut1/PattayaFloodRiskManagement/blob/main/ML_Model/FloodRisk_ML_BGDT.pkl?raw=true'
-mfile = BytesIO(requests.get(mLink).content)
-loadmodel=joblib.load(mfile)
-predictEX= loadmodel.predict(X)
 
+with content3:
+	st.markdown("<h4>Prediction</h4>", unsafe_allow_html=True)
+	data = pd.DataFrame(columns=['Annual_Rain', 'Soil_Drainage_Capacity', 'Elevation', 'Degree_Slope', 'Landuse', 'FAR', 'BCR'])
+
+	data = data.append({
+        'Annual_Rain': Annual_Rain,
+        'Soil_Drainage_Capacity': Soil_Drainage_Capacity,
+        'Elevation': Elevation,
+        'Degree_Slope': Degree_Slope,
+        'Landuse': Landuse,
+        'FAR': FAR,
+        'BCR': BCR
+    }, ignore_index=True)
+
+	st.dataframe(data)
+
+
+	feature_cols=['Annual_Rain','Elevation','Degree_Slope','Landuse','Soil_Drainage_Capacity','BCR','FAR']
+
+	X=data[feature_cols]
+	mLink = 'https://github.com/atomsiwawut1/PattayaFloodRiskManagement/blob/main/ML_Model/FloodRisk_ML_BGDT.pkl?raw=true'
+	mfile = BytesIO(requests.get(mLink).content)
+	loadmodel=joblib.load(mfile)
+	predictEX= loadmodel.predict(X)
+	text = str(predictEX[0]) 
+	#st.write(text)
+
+
+if text == 'เสี่ยงภัยมาก':
+    gauge_value = 100
+    gauge_min = 0
+    gauge_max = 100
+    gauge_label = 'เสี่ยงภัยมาก'
+    gauge_color = 'red'
+elif text == 'เสี่ยงภัยปานกลาง':
+    gauge_value = 50
+    gauge_min = 0
+    gauge_max = 100
+    gauge_label = 'เสี่ยงภัยปานกลาง'
+    gauge_color = 'orange'
+elif text == 'เสี่ยงภัยน้อย':
+    gauge_value = 30
+    gauge_min = 0
+    gauge_max = 100
+    gauge_label = 'เสี่ยงภัยน้อย'
+    gauge_color = 'yellow'
+else:
+    gauge_value = 0
+    gauge_min = 0
+    gauge_max = 100
+    gauge_label = 'ไม่มีความเสี่ยง'
+    gauge_color = 'green'
+
+
+
+# Create the Gauge Chart using plotly
+fig = go.Figure(go.Indicator(
+    mode='gauge+number',
+    value=gauge_value,
+    domain={'x': [0, 1], 'y': [0, 1]},
+    gauge={'axis': {'range': [gauge_min, gauge_max]},
+           'bar': {'color': gauge_color},
+           'steps': [
+               {'range': [gauge_min, gauge_max], 'color': 'lightgray'}],
+           'threshold': {
+               'line': {'color': 'red', 'width': 4},
+               'thickness': 0.75,
+               'value': gauge_value}
+           },
+    title={'text': gauge_label}))
+
+#fig.update_layout(height=400)
+
+fig.update_layout(height=400)
+# Display the Gauge Chart
+content3.plotly_chart(fig)
                
 
-st.write(predictEX)        
+      
 
 
 
